d619efd8c96bb31f860454087fb063d1d2768b11
Ajay Jaiswar <ajayvjaiswar@gmail.com>
2020-10-30 20:28:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http'; 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { CloudFirestoreComponent } from './cloud-firestore/cloud-firestore.component';
+import { RealTimeDatabaseComponent } from './real-time-database/real-time-database.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CloudFirestoreComponent,
+    RealTimeDatabaseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,  
+    HttpClientModule,  
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCQlVHok6jyZOkSQbQp9x1E1PGD_m5aIDU",
+    authDomain: "ng-fb-crud-6fee9.firebaseapp.com",
+    databaseURL: "https://ng-fb-crud-6fee9.firebaseio.com",
+    projectId: "ng-fb-crud-6fee9",
+    storageBucket: "ng-fb-crud-6fee9.appspot.com",
+    messagingSenderId: "323247589764",
+    appId: "1:323247589764:web:a77c11c53f31c34117b5d1",
+    measurementId: "G-RM2E61Z6VX"
+  }
 };
 
 /*
